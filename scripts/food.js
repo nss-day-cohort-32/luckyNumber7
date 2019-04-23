3dc1c8085d53570deba48039a772d7aafbc4f4ad
Dan Storm <stormdanp@gmail.com>
2019-04-22 20:08:12 -0500

    cleaned up JS. Let's see if it works... Should have committed this after I tested it. FAILURE IS LEARNING

--- scripts/food.js
+++ scripts/food.js
@@ -4,31 +4,18 @@ console.log(cuisineTypeNumber);
 
 
 //select a cuisine type
-foodOptions.onchange = function (){
+foodOptions.onchange () => {
     cuisineTypeNumber = foodOptions.options[foodOptions.selectedIndex].value;
     console.log(cuisineTypeNumber);
 }
 
 // fetch restaurant data
-fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&count=4&cuisines=${option.value}`,{
+fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&count=4&cuisines=${cuisineTypeNumber}`,{
 "headers": {
   "user-key": "ce5dc477c3311b85af742db4ad454c90",
 }})
         .then(response => response.json())
-        .then(parsedFoods => {
+        .then(restaurantChoices => {
 //console.table represents data in console as table
-            console.table(parsedFoods);
-//loop over local food data, grab barcode and use it to fetch API data
-            parsedFoods.forEach(cuisine => {
-                fetch(
-                    "https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&count=4&cuisines=148"
-                )
-                console.log("cuisine",cuisine)
-                .then(APIfoods => APIfoods.json())
-                .then(parsedAPIfoods => {
-//target HTML element and inject DOM element created by foodFactory function
-                    foodList.innerHTML += foodFactory(food, parsedAPIfoods);
-                });
-            });
-        });
-
+            console.table(restaurantChoices);
+        })
