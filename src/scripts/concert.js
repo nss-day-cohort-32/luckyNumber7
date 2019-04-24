5a301dcee675f6d521497a0444cd952964f4b468
jksiggy <47400504+jksiggy@users.noreply.github.com>
2019-04-24 14:14:35 -0500

    no changes

--- src/scripts/concert.js
+++ src/scripts/concert.js
@@ -1,12 +1,21 @@
 
-let concertDate = {
-cocertsEvents(userInput) {
+// let concertDate = {
+// cocertsEvents(userInput) {
 
-fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=XDZ7nd4kIfrX7NgbDW8U1TVk09GslZnh&city=Nashville&classificationName=music&keyword=${userInput}`)
+let concertInput = document.querySelector("#concerts-search")
+
+
+let concertButton = document.querySelector("#concerts-search-button")
+concertButton.addEventListener("click", () => {
+  input(concertInput.value)
+})
+                         
+function input(Genre) {
+fetch(`https://app.ticketmaster.com/discovery/v2/events?apikey=XDZ7nd4kIfrX7NgbDW8U1TVk09GslZnh&keyword=${Genre}&city=nashville&countryCode=US`)
  .then(events =>events.json())
  .then(parsedEvents => {
     console.log(parsedEvents)
-     const events = parsedEvents._embedded.events;
+     let events = parsedEvents._embedded.events;
     events.forEach(element => {
         
          console.log(element.name);
@@ -14,18 +23,16 @@ fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=XDZ7nd4kIfrX
          console.log(element.dates.start.localDate)
          console.log(element.classifications[0].genre.name)
          
-         let eventsName = element.name
-        let eventsDates = element.dates.start.localDate
-        let eventsGenre = element.classifications[0].genre.name
-        let concerthtml = document.querySelector("#eventsOptions")
+      //    let eventsName = element.name
+      //   let eventsDates = element.dates.start.localDate
+      //   let eventsGenre = element.classifications[0].genre.name
+         let concerthtml = document.querySelector("#concertsResult")
          concerthtml.innerHTML += 
          `<h2>${eventsName}</h2>
-        <p>${eventsDates}</p>
-      <p>${eventsGenre}</p>`
+         <p>${eventsDates}</p>
+       <p>${eventsGenre}</p>`
         
 
  })
   })
-  
-}
 }
